d2736bc1e7ad3ea49aef0b54d04f8e61f5e0a215
Thilak <thilak.ram@thoughtworks.com>
2021-03-02 16:19:37 +0530

    [Aniruth | Thilak] [Covid 19 - Program Status Report] Add last name to the patient name column

--- openmrs/apps/reports/sql/covid19StatusReport.sql
+++ openmrs/apps/reports/sql/covid19StatusReport.sql
@@ -1,12 +1,15 @@
 SELECT
-    person_name.given_name AS `Patient Name`, 
+    CONCAT(person_name.given_name, ' ', person_name.family_name) AS `Patient Name`, 
     patient_program.date_enrolled AS `Date Enrolled in Program`, 
     dateDiff(IFNULL(patient_program.date_completed, now()), patient_program.date_enrolled) AS `No. of Days in Program`,  
     patient_state.state AS `Current State in Program`
 FROM patient_program 
     INNER JOIN program ON patient_program.program_id=program.program_id
-    INNER JOIN patient_state ON patient_program.patient_program_id=patient_state.patient_program_id 
     INNER JOIN person_name ON person_name.person_id=patient_program.patient_id
-WHERE program.name="COVID-19 Program" 
-GROUP BY person_name.given_name
-ORDER BY patient_program.date_enrolled DESC
+    INNER JOIN patient_state ON patient_program.patient_program_id=patient_state.patient_program_id
+WHERE 
+    program.name="COVID-19 Program" 
+GROUP BY 
+    person_name.given_name
+ORDER BY 
+    patient_program.date_enrolled DESC;
