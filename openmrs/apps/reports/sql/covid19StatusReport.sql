fd8f803d51eec536c629808bc25a42c2e254b3f8
Thilak <thilak.ram@thoughtworks.com>
2021-03-02 18:08:11 +0530

    [Aniruth | Thilak] [Covid 19 - Program Status Report] Refactor state id to state name

--- openmrs/apps/reports/sql/covid19StatusReport.sql
+++ openmrs/apps/reports/sql/covid19StatusReport.sql
@@ -1,15 +1,15 @@
-SELECT
-    CONCAT(person_name.given_name, ' ', person_name.family_name) AS `Patient Name`, 
-    patient_program.date_enrolled AS `Date Enrolled in Program`, 
-    dateDiff(IFNULL(patient_program.date_completed, now()), patient_program.date_enrolled) AS `No. of Days in Program`,  
-    patient_state.state AS `Current State in Program`
-FROM patient_program 
-    INNER JOIN program ON patient_program.program_id=program.program_id
-    INNER JOIN person_name ON person_name.person_id=patient_program.patient_id
-    INNER JOIN patient_state ON patient_program.patient_program_id=patient_state.patient_program_id
-WHERE 
-    program.name="COVID-19 Program" 
-GROUP BY 
-    person_name.given_name
-ORDER BY 
-    patient_program.date_enrolled DESC;
+SELECT 
+	CONCAT(pn.given_name, ' ', pn.family_name) AS `Person Name`,
+	DATE(MAX(pp.date_enrolled)) AS `Date Enrolled in Program`,
+    DATEDIFF(IFNULL(pp.date_completed, NOW()), pp.date_enrolled) AS `No. of Days in Program`,
+	cn.name AS `Current State in Program`
+FROM patient pt
+    INNER JOIN person_name pn ON pt.patient_id = pn.person_id
+    INNER JOIN patient_program pp ON pt.patient_id = pp.patient_id
+    INNER JOIN patient_state ps ON ps.patient_program_id = pp.patient_program_id
+    INNER JOIN program_workflow_state pws ON pws.program_workflow_state_id = ps.state
+    INNER JOIN concept_name cn ON cn.concept_id = pws.concept_id
+    INNER JOIN obs obs ON obs.person_id = pt.patient_id
+    INNER JOIN program pr ON pr.program_id = pp.program_id
+WHERE cn.concept_name_type = "SHORT" AND pr.name = "COVID-19 Program"
+GROUP BY pn.person_id;
